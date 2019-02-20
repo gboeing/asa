ca843eaf5db4c5b55b2bbd7849c0c5458e698ca7
gboeing <g.boeing@northeastern.edu>
2019-02-20 17:02:07 -0500

    update env script

--- environment/create-windows-env.bat
+++ environment/create-windows-env.bat
@@ -1,4 +1,6 @@
 CALL conda create -n asa -c conda-forge --yes python=3 --file requirements.txt
 CALL activate asa
+CALL python -m ipykernel install --user
 CALL jupyter labextension install @jupyter-widgets/jupyterlab-manager
 CALL jupyter labextension install jupyter-leaflet
+CALL conda clean --all --yes
