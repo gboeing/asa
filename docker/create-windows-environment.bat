67817863c2aaa0fe973b7ea75a01aeee5e99fd81
gboeing <g.boeing@northeastern.edu>
2019-02-21 14:34:52 -0500

    update docker/env

--- docker/create-windows-environment.bat
+++ docker/create-windows-environment.bat
@@ -4,4 +4,4 @@ CALL python -m ipykernel install --user
 CALL jupyter labextension install @jupyter-widgets/jupyterlab-manager
 CALL jupyter labextension install jupyter-leaflet
 CALL conda clean --all --yes
-CALL conda deactivate asa
+CALL conda deactivate
