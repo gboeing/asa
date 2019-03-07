ead50e85da0a92310a3331b02cf51ad5d813692d
gboeing <g.boeing@northeastern.edu>
2019-03-07 17:07:24 -0500

    update docker

--- docker/create-windows-environment.bat
+++
@@ -1,7 +0,0 @@
-CALL conda create -n asa -c conda-forge --yes --file requirements.txt
-CALL conda activate asa
-CALL python -m ipykernel install --user
-CALL jupyter labextension install @jupyter-widgets/jupyterlab-manager
-CALL jupyter labextension install jupyter-leaflet
-CALL conda clean --all --yes
-CALL conda deactivate
