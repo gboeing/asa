ab2c049574f4cf534fd2227526cabfefe748d6f9
gboeing <g.boeing@northeastern.edu>
2019-03-13 12:49:33 -0400

    update env

--- docker/windows-env/create-windows-environment.bat
+++ docker/windows-env/create-windows-environment.bat
@@ -3,9 +3,11 @@ CALL conda deactivate
 CALL conda env remove -n asa --yes
 CALL conda create -n asa -c conda-forge --file "../requirements.txt" --yes
 CALL conda activate asa
-CALL python -m ipykernel install --user
+CALL python -m ipykernel install --user --name asa --display-name "Python (asa)"
 CALL jupyter labextension install @jupyter-widgets/jupyterlab-manager
 CALL jupyter labextension install jupyter-leaflet
 CALL conda clean --all --yes
 CALL conda env export > environment-windows.yml
+CALL conda list
+CALL jupyter kernelspec list
 CALL python -c "import osmnx; print(osmnx.__version__)"
